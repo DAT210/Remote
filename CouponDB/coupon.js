e17e433188fa748853d5ebdbddbc471ccb157b7f
Grunde3 <42542048+Grunde3@users.noreply.github.com>
2018-10-04 09:30:46 +0200

    pathing fix
    Error: SQLITE_ERROR: incomplete input

--- CouponDB/coupon.js
+++ CouponDB/coupon.js
@@ -1,26 +1,35 @@
 var sqlite3 = require('sqlite3').verbose();
 // open the database
 
-var db = new sqlite3.Database('.CouponDB.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the Coupon database.');
+var path = require('path')
+var dbPath = path.resolve(__dirname, 'CouponDB.db')
+var db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE , (err) => {
+  if (err){console.error(err.message);
+  db.close();}
+  console.log('Connected')
 });
+
+// var db = new sqlite3.Database('CouponDB\CouponDB.db', sqlite3.OPEN_READWRITE, (err) => {
+  //if (err) {
+ //   console.error(err.message);
+ //   db.close();
+ // }
+//  console.log('Connected to the Coupon database.');
+//});
  
 db.serialize(function() {
-  var starter = db.prepare("INSERT INTO UserID Values");
+  var starter = db.prepare("INSERT INTO User Values");
   for (var i=0;i<10;i++){
     var n = 9000+i;
     starter.run(i,n)
   }
-  starter.finalize;
+  starter.finalize();
 
   db.each(`SELECT UserId FROM User`, function (err, row){
     if (err) {
       console.error(err.message);
     }
-    console.log(row.id + "\t" + row.first_name);
+    console.log(row.id + "\t");
   });
 });
  
