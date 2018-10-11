755f0132862a96ec082a1cc060ac0e64f5fe425e
Grunde3 <42542048+Grunde3@users.noreply.github.com>
2018-10-11 19:19:16 +0200

    CouponDB rev.
    revidert

--- CouponDB/coupon.js
+++ CouponDB/coupon.js
@@ -5,33 +5,51 @@ var path = require('path')
 var dbPath = path.resolve(__dirname, 'CouponDB.db')
 var db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE , (err) => {
   if (err){console.error(err.message);
-  db.close();}
+  }
   console.log('Connected')
 });
 
-// var db = new sqlite3.Database('CouponDB\CouponDB.db', sqlite3.OPEN_READWRITE, (err) => {
-  //if (err) {
- //   console.error(err.message);
- //   db.close();
- // }
-//  console.log('Connected to the Coupon database.');
-//});
+db.run('CREATE TABLE IF NOT EXISTS User(UserID INTEGER PRIMARY KEY, coupon INTEGER)', function(err) {
+	if (err) {
+    console.log(err.message);
+  }
+	});
+
+ db.run('CREATE TABLE IF NOT EXISTS UserIDs(UserID INTEGER PRIMARY KEY)', function(err){if (err){
+   console.log(err.message);
+ }});
+
+var d= Date.now ;
+var datenow = d.toString();
+
+ db.run('CREATE TABLE IF NOT EXISTS coupons(couponID integer PRIMARY KEY AUTO_INCREMENT, expiration_date text, type integer, value integer) VALUES (?, datenow , 1, 50  )', function(err){if (err){
+   console.log(err.message);
+ }
+});
+ 
  
 db.serialize(function() {
-  var starter = db.prepare("INSERT INTO User Values");
-  for (var i=0;i<10;i++){
-    var n = 9000+i;
-    starter.run(i,n)
-  }
+  var starter = db.prepare('INSERT INTO User (UserID, coupon) VALUES(?,?) ', function(err) {
+    if (err) {
+      return console.log(err.message);
+    };
+    for (var i=1; i++; i<10 ){
+    var n = i;
+    starter.run(i,i+100)
+
+    };
+  });
   starter.finalize();
 
-  db.each(`SELECT UserId FROM User`, function (err, row){
+  db.each(`SELECT UserID, coupon FROM User`, function (err, row){
     if (err) {
       console.error(err.message);
     }
-    console.log(row.id + "\t");
+    console.log(row.UserID + row.coupon);
   });
 });
+
+
  
 db.close((err) => {
   if (err) {
@@ -39,3 +57,4 @@ db.close((err) => {
   }
   console.log('Close the database connection.');
 });
+
