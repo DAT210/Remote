697c1d9a2630bb008b320a98e0952b86b86f445f
fredtvin <35257039+fredtvin@users.noreply.github.com>
2018-10-30 08:12:42 +0100

    Feil safe (#7)
    * fixes
    
    * fix stringify
    
    * fix
    
    * fixes
    
    * fixes
    
    * fixes^fixes
    
    * fixes^fixes2

--- src/app.js
+++ src/app.js
@@ -43,29 +43,22 @@ const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database(path.resolve(__dirname, `../db/${process.env.DATABASE_MEALDEALS_NAME}`));
 
 
-db.run('CREATE TABLE IF NOT EXISTS TokensDB(UserId INTEFER PRIMARY KEY, Tokens INTEGER, GameTime TEXT)',function(err){
+db.run('CREATE TABLE IF NOT EXISTS TokensTable(UserID INTEGER PRIMARY KEY, Tokens INTEGER, NextPlayDate TEXT)',function(err){
 	if(err){
 		console.log(err.message);
 	}
 });
-db.run('CREATE TABLE IF NOT EXISTS MealDeals(DealId INTEGER PRIMARY KEY, Price INTEGER, Name TEXT, StartDate TEXT, EndDate TEXT)', function(err) {
+db.run('CREATE TABLE IF NOT EXISTS MealDeals(DealID INTEGER PRIMARY KEY, Price INTEGER, Name TEXT, StartDate TEXT, EndDate TEXT)', function(err) {
 	if (err) {
 		console.log(err.message);
 	}
 });
 
-db.run('CREATE TABLE IF NOT EXISTS Courses(DealId INTEGER, CourseId INTEGER, NumberOfItems INTEGER, FOREIGN KEY(DealId) REFERENCES MealDeals(DealId), PRIMARY KEY(DealId, courseId))', function(err) {
+db.run('CREATE TABLE IF NOT EXISTS Courses(DealID INTEGER, CourseID INTEGER, NumberOfItems INTEGER, FOREIGN KEY(DealID) REFERENCES MealDeals(DealID), PRIMARY KEY(DealID, courseID))', function(err) {
 	if (err) {
 		console.log(err.message);
 	}
 });
-
-db.run('CREATE TABLE IF NOT EXISTS MealDeals_Connection(DealId INTEGER, CourseId INTEGER, RowId INTEGER PRIMARY KEY)', function(err) {
-	if (err) {
-		console.log(err.message);
-	}
-});
-
 
 const app = express();
 const port = process.env.PORT;
@@ -98,7 +91,7 @@ app.use(bodyParser.json());
 	Code 404 means that something is missing
 	
 	{
-		dealId: "",
+		dealID: "",
 		price: "",
 		name: "",
 		startDate: "",
@@ -115,25 +108,64 @@ function postDeal(req,res){
 		descriptionTestVar = false;
 
 		//test if products exists insert here
-		dbMealDeal(req,res)
+		dbMealDeal(req,res);
 		
 	}else{
-		resp.description = "course: " + resp.description + " does not exist" 
-		res.status(400).json(resp)
+		resp.description = "course: " + resp.description + " does not exist";
+		res.status(400).json(resp);
 	}
 }
 
+function isInt(value) {
+  var x;
+  return isNaN(value) ? !1 : (x = parseFloat(value), (0 | x) === x);
+}
+function isString(value){
+	console.log(/\S/.test(value))
+	if(!(/\S/.test(value)))return true;
+	else if(value === undefined)return true;
+	
+	return false;
+}
 
 function dbMealDeal(req,res){
 	let json = req.body;
-	let mealdealid = parseInt(json.dealId, 10);
+	let mealdealid = parseInt(json.dealID, 10);
 	let price = parseInt(json.price, 10);
 	let name = json.name;
 	let startdate = json.startDate;
 	let enddate = json.endDate;
 	let test = false;
-	console.log(`INSERT INTO MealDeals(DealId, Price, Name, StartDate, EndDate) VALUES (${mealdealid}, ${price}, "${name}", "${startdate}", "${enddate}")`)
-	db.run(`INSERT INTO MealDeals(DealId, Price, Name, StartDate, EndDate) VALUES (${mealdealid}, ${price}, "${name}", "${startdate}", "${enddate}")`, function(err) {
+	let inputError = false;
+	if (isInt(mealdealid) == false) {
+		inputError = true;
+	}else if(isInt(price) == false){
+		inputError = true;
+	}else if (isString(name)){
+		inputError = true;
+	}else if (isString(startdate)){
+		inputError = true;
+	}else if (isString(enddate)){
+		inputError = true;
+	}
+		
+	if(json.courses.length > 0 ){
+		for(let counter = 0; counter < json.courses.length; counter++){
+			if(isInt(json.courses[counter].numberOfItems) == false) {
+				inputError = true;
+			}
+			if(isInt(json.courses[counter].courseID) 	 == false){
+				inputError = true;
+			}
+		}
+	}
+	if (inputError){
+		let resp = JSON.parse('{}')
+		resp.message = "input is wroung";
+		res.status(400).json(resp);
+		return;
+	}
+	db.run(`INSERT INTO MealDeals(DealID, Price, Name, StartDate, EndDate) VALUES (${mealdealid}, ${price}, "${name}", "${startdate}", "${enddate}")`, function(err) {
 		if (err) {
 			let resp = JSON.parse('{}');
 			console.log(err.message);			
@@ -153,20 +185,19 @@ function dbMealDeal(req,res){
 */
 function insertCourses(res,json){
 	let courses = json.courses;
-	let mealdealid = parseInt(json.dealId, 10);
+	let mealdealid = parseInt(json.dealID, 10);
+	var BreakException = {};
 	for(let counter = 0; counter < courses.length; counter++){
-		courseid = parseInt(courses[counter].courseId);
+		courseid = parseInt(courses[counter].courseID);
 		numberofitems = parseInt(courses[counter].numberOfItems);
-		db.run(`INSERT INTO Courses(DealId, CourseId, NumberOfItems) VALUES (${mealdealid}, ${courseid}, ${numberofitems})`, function(err) {
+		db.run(`INSERT INTO Courses(DealID, CourseID, NumberOfItems) VALUES (${mealdealid}, ${courseid}, ${numberofitems})`, function(err) {
 			if (err) {
 				let resp = JSON.parse('{}');
 				console.log(err.message);
 				resp.message = "Could not create the MealDeals_Connection";
 				resp.description = err.message + " insert into MealDeals_Connection";
-				res.status(400).json(resp);
-				throw BreakException;
-			}
-			else{
+				res.status(400).json(resp);	
+			}else{
 				res.status(201).end();
 			}
 		});
@@ -182,54 +213,49 @@ function insertCourses(res,json){
 */
 
 function getDeal(res,id){
-	db.all(`SELECT * FROM Courses`, function(err,rows){
+	let resp = JSON.parse('{}');
+	db.get(`SELECT * FROM MealDeals WHERE DealID = ${id}`, function(err, row){
 		if(err){
-			
-		}else console.log(rows)
-	})
-	db.get(`SELECT * FROM MealDeals WHERE DealId = ${id}`, function(err, row){
-		if(err){
-			
-		}else{
-			if(!(row === undefined)){
-				db.all(`SELECT * FROM Courses WHERE DealId = ${row.DealId}`, function(err,rows2){
-					if(err){
-					}else{
-						if (rows2.length >0){
-							all_CourseId = [];
-							rows2.forEach(function(row2){
-							all_CourseId.push([row2.CourseId,row2.NumberOfItems]);
-						});
-							rows2[0].Course_Id = all_CourseId;
-							res.status(200).json(rows2[0]);
-						}else res.status(404).end();
-					}
-				});
-			}else res.status(404).end();
-		}
-	});
-}
-
-/*
-	Gets the connections between MealDeals and Courses
-
-	Response format, JSON
-	Valid response with data has status code 200.
-	Code 400 means that somethig went wrong 
-*/
-
-function mealDealConn(res){
-	db.all(`SELECT * FROM MealDeals_Connection`, function(err,row){
-		if(err){
-			let resp = JSON.parse('{}');
-			resp.message = "Could not get MealDealsConnention";
+			resp.message = "Could not get MealDeals";
 			resp.description = err.message;
 			res.status(400).json(resp);
 		}else{
-			res.status(200).json(row);
+			if(!(row === undefined)){
+				db.all(`SELECT * FROM Courses WHERE DealID = ${row.DealID}`, function(err2,rows2){
+					if(err2){
+						resp.message = "Could not get Courses";
+						resp.description = err2.message;
+						res.status(400).json(resp);
+					}else{
+						if (rows2.length >0){
+							all_CourseID = [];
+							row.Courses = [];
+							for(let counter = 0; counter < rows2.length; counter++){
+								row.Courses[counter] = {"courseID": rows2[counter].CourseID, "numberOfItems": rows2[counter].NumberOfItems };
+							}
+							
+							res.status(200).json({
+								"dealID": row.DealID,
+								"price": row.Price,
+								"name": row.Name,
+								"startDate": row.StartDate,
+								"endDate": row.EndDate,
+								"courses": row.Courses
+							});
+						}else{
+							resp.message = "No Course with mealDealid";
+							res.status(404).json(resp);
+						}
+					}
+				});
+			}else{
+				resp.message = "No MealDeal with this id";
+				res.status(404).json(resp);
+			}
 		}
 	});
 }
+
 /*
 	Gets the course
 
@@ -238,57 +264,36 @@ function mealDealConn(res){
 	Code 404 means that the course does not exist
 */
 
-function getCourse(res,id){
-	
-	db.get(`SELECT * FROM Courses WHERE Course_Id = ${id}`, function(err, row){
-		if (err){
-			let resp = JSON.parse('{}');
-			resp.message = "Could not get the course";
-			resp.description = err.messagepo;
-			res.status(400).json(resp);
-		} else{
-			if (row == undefined){
-				let resp = JSON.parse('{}');
-				resp.message = "Course does not exsist";
-				res.status(404).json(resp);
-			}
-			res.status(200).json(row);
-		}
-	});
-}
-
 app.post('/rewards', function(req, res){
 	let page = req.body.page;
-	if (!['courses','mealDeal'].includes(page)){
+	if (!['mealDeal'].includes(page)){
 		console.log('Not a valid page');
 		res.status(404).end();
-		return
+		return;
 	}
 	console.log(page)
 	if (page === 'mealDeal'){
 		postDeal(req,res);
-	}else if(page === 'courses'){
-		postCourse(req,res);
 	}
 });
 
 app.get('/reward-pages/:id', function(req, res){
 	let page = req.query.page;
-	if (!['tokens','courses','mealDeal'].includes(page)){
+	if (!['tokens','courses','mealDeal', 'nextPlayDate'].includes(page)){
 		console.log('Not a valid page');
 		res.status(404).end();
-		return
+		return;
 	}
 	
 	let id = parseInt(req.params.id, 10);
 	if (page === 'tokens'){
 		getTokens(res,id);
-	}else if(page === 'courses'){
-		getCourse(res,id);
 	}else if(page === 'mealDeal'){
 		getDeal(res,id);
 	}else if(page === 'mealDealConn'){
 		mealDealConn(res);
+	}else if(page === 'nextPlayDate'){
+		getNextPlayDate(res,id);
 	}
 });
 
@@ -301,9 +306,8 @@ app.get('/reward-pages/:id', function(req, res){
 */
 
 function getTokens(res, id){
-	//ID = parseInt(req.params.id,10);
 	
-	db.get(`SELECT Tokens from TokensDB WHERE UserId = ${id}`, function(err, row){
+	db.get(`SELECT Tokens, NextPlayDate from TokensTable WHERE UserID = ${id}`, function(err, row){
 		if(err){
 			let resp = JSON.parse('{}');
 			resp.message = "Could not get User";
@@ -311,18 +315,21 @@ function getTokens(res, id){
 		}
 		else{
 			if (!(row == undefined)){
-				res.status(200).json(JSON.stringify(row));
+				res.status(200).json({
+					"tokens": row.Tokens,
+					"nextPlayDate": row.NextPlayDate
+				});
 			}else{
-				db.run(`INSERT INTO TokensDB(UserId, Tokens) VALUES (${id}, 0)`, function(err){
+				db.run(`INSERT INTO TokensTable(UserID, Tokens, NextPlayDate) VALUES (${id}, 0, "")`, function(err){
 					if(err){
 						let resp = JSON.parse('{}');
 						resp.message = "Could not create User";
 						res.status(400).json(resp);
 					}else{
-						res.status(200).json(JSON.stringify({
-							"UserId": id,
-							"Tokens": '0'
-						}));
+						res.status(200).json({
+							"tokens": 0,
+							"nextPlayDate": ""
+							});
 					}
 				});
 			}
@@ -337,15 +344,19 @@ function getTokens(res, id){
 	Valid response with data has status code 200.
 	Code 400 means that somethig went wrong 
 	{
-		"user_Id Int"
-		"tokens "
+		"userID":  Int
+		"tokens":  Int
 	}
 */
 app.patch('/addTokens', function(req,res){
 	let json = req.body;
-	let userid = parseInt(json.UserId, 10);
-	let tokens = parseInt(json.Tokens, 10);
-	db.get(`SELECT Tokens FROM TokensDB WHERE UserId = ${userid}`, function(err,row){
+	
+	let userid = parseInt(json.userID, 10);
+	let tokens = parseInt(json.tokens, 10);
+	if (!(json.nextPlayDate === undefined)){
+		patchNextPlayDate(req,res);
+	}
+	db.get(`SELECT Tokens FROM TokensTable WHERE UserID = ${userid}`, function(err,row){
 		if(err){
 			console.log(err.message);			
 			let resp = JSON.parse('{}');
@@ -355,12 +366,12 @@ app.patch('/addTokens', function(req,res){
 		}else{	
 			if(row === undefined){
 				let resp = JSON.parse('{}');
-				resp.message = "User does not exist"
+				resp.message = "User does not exist";
 				res.status(404).json(resp);
 				return;
 			}
 			let TokensInput = row.Tokens + tokens;
-			db.run(`UPDATE TokensDB SET Tokens = ${TokensInput} WHERE UserId = ${userid}`,function(err){
+			db.run(`UPDATE TokensTable SET Tokens = ${TokensInput} WHERE UserID = ${userid}`,function(err){
 				if (err){
 					console.log(err.message);			
 					let resp = JSON.parse('{}');
@@ -375,41 +386,41 @@ app.patch('/addTokens', function(req,res){
 	})
 });
 
-app.patch('/subTokens', function(req, res){
-	let json = req.body;
-	let userid = parseInt(json.UserId, 10);
-	let tokens = parseInt(json.Tokens, 10);
-	db.get(`SELECT Tokens FROM TokensDB WHERE UserId = ${userid}`, function(err,row){
-		if(err){
-			console.log(err.message);			
+function getNextPlayDate(res,id){
+	db.get(`SELECT NextPlayDate From TokensTable WHERE UserID = ${id}`, function(err, row){
+		if (err){
+			console.log(err.message);
 			let resp = JSON.parse('{}');
-			resp.message = "Could not get information from database";
+			resp.message = "Could not open database";
 			resp.description = err.message;
 			res.status(400).json(resp);
-		}else{	
-			if(row === undefined){
+		}else{
+			if (row === undefined){
 				let resp = JSON.parse('{}');
-				resp.message = "User does not exist"
+				resp.message = "User does not exist";
 				res.status(404).json(resp);
-				return;
+			}else{
+				res.status(200).json({"gameTime": row.GameTime});
 			}
-			let TokensInput = row.Tokens - tokens;
-			db.run(`UPDATE TokensDB SET Tokens = ${TokensInput} WHERE UserId = ${userid}`,function(err){
-				if (err){
-					console.log(err.message);			
-					let resp = JSON.parse('{}');
-					resp.message = "Could not update Database";
-					resp.description = err.message;
-					res.status(400).json(resp);
-				}else{
-					res.status(200).end();
-				}
-			});
 		}
-	})
-});
+	});
+}
 
-
+function patchNextPlayDate(req,res){
+	let userid = parseInt(req.body.userID,10);
+	let nextplaydate = req.body.nextPlayDate;
+	db.run(`UPDATE TokensTable SET NextPlayDate = "${nextplaydate}" WHERE UserID = ${userid}`, function(err){
+		if (err){
+			console.log(err.message);			
+			let resp = JSON.parse('{}');
+			resp.message = "Could not update Database";
+			resp.description = err.message;
+			res.status(400).json(resp);
+		}else{
+			res.status(200).end();
+		}
+	});
+}
 /*
 	test if the server can start 
 */
